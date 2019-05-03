d11c550ae422d050169c46e1570803fa3056635b
golden <golden@example.com>
2019-05-03 19:27:22 +0800

    重构项目目录结构

--- miya-common-dao/src/main/java/com/miya/dto/Converter.java
+++
@@ -1,8 +0,0 @@
-package com.miya.dto;
-
-import java.lang.reflect.InvocationTargetException;
-
-public abstract  class Converter<S,T> {
-    protected abstract   T doForward(S s) throws InvocationTargetException, IllegalAccessException;
-    protected  abstract  S doBackward(T t) throws InvocationTargetException, IllegalAccessException;
-}
--- miya-common-dao/src/main/java/com/miya/entity/dao/ItemCatDao.java
+++
@@ -1,9 +0,0 @@
-package com.miya.entity.dao;
-
-import com.miya.entity.ItemCat;
-import com.springboot.ping.mybatis.extend.dao.BaseCURDDao;
-
-public interface ItemCatDao extends BaseCURDDao<ItemCat> {
-
-
-}
